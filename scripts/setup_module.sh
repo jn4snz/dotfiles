efb677ecf81740eb45265769221413854b1dac36
L290849 <l290849@pn1714459.lanl.gov>
2017-01-31 12:47:32 -0700

    Cleanup stuff that I don't use, start from scratch

--- scripts/setup_module.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-GREP=`which grep`
-NEED_TO_SOURCE=`hostname | ${GREP} '.lanl.gov'`
-MACOSX=`uname | ${GREP} 'Darwin'`
-if [ -n "${NEED_TO_SOURCE}" ]; then
-  if [ -z "${MACOSX}" ]; then
-    source /usr/share/Modules/init/bash
-  fi
-fi
